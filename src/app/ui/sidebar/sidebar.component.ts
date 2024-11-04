ef0377fd008912ae613d74855834d9620c2bcc6c
nashuar <nashuarash@gmail.com>
2024-11-04 15:07:37 +0300

    chore: upd sidebar

--- src/app/ui/sidebar/sidebar.component.ts
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent, NgFor],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Home',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Chat',
+      icon: 'chat',
+      link: '',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      link: '',
+    },
+  ];
 }
